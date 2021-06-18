d8afcc1b18e5380b89afde4275c43a7e850d3377
Justine Loubry <justineloubry@gmail.com>
2021-06-18 14:29:20 +0200

    bottom nav fixed

--- app/src/main/java/com/example/kade_submission_2/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/kade_submission_2/activity/SplashScreenActivity.kt
@@ -3,7 +3,6 @@ package com.example.kade_submission_2.activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
